1e8cfbda9ec14709ad98312989b53ee2852c1942
piotrexgwi <79444557+piotrexgwi@users.noreply.github.com>
2021-03-08 09:13:57 +0100

    Add files via upload

---
+++ mainwindow.h
@@ -0,0 +1,24 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+    Dialog *a;
+    Dialog2 *b;
+    Dialog3 *c;
+};
+#endif // MAINWINDOW_H
